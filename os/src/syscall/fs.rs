6827b6afdd00f9320c585c0de8bc403018520e20
martin.cong <martin.cong@zhulinkeji.com>
2024-10-20 23:19:37 +0800

    debug comment

--- os/src/syscall/fs.rs
+++ os/src/syscall/fs.rs
@@ -10,6 +10,8 @@ pub fn sys_write(fd: usize, buf: *const u8, len: usize) -> isize {
     trace!("kernel: sys_write");
     match fd {
         FD_STDOUT => {
+            // buf 是用户空间的虚拟地址，需要转换一下
+            // 将每个字节数组切片转化为字符串 &str 然后输出即可
             let buffers = translated_byte_buffer(current_user_token(), buf, len);
             for buffer in buffers {
                 print!("{}", core::str::from_utf8(buffer).unwrap());
--- os/src/trap/mod.rs
+++ os/src/trap/mod.rs
@@ -56,7 +56,11 @@ pub fn enable_timer_interrupt() {
 /// trap handler
 #[no_mangle]
 pub fn trap_handler() -> ! {
+    //将 stvec 修改为同模块下另一个函数 trap_from_kernel 的地址。这就是说，一旦进入内核后再次触发到 S 的 Trap，
+    // 则会在硬件设置一些 CSR 之后跳过寄存器 的保存过程直接跳转到 trap_from_kernel 函数，在这里我们直接 panic 退出
     set_kernel_trap_entry();
+    // 由于应用的 Trap 上下文不在内核地址空间，因此我们调用 current_trap_cx 来获取当前应用的 Trap 上下文的可变引用
+    // 而不是像之前那样作为参数传入 trap_handler 
     let cx = current_trap_cx();
     let scause = scause::read(); // get trap cause
     let stval = stval::read(); // get extra value
@@ -101,7 +105,12 @@ pub fn trap_handler() -> ! {
 /// set the reg a0 = trap_cx_ptr, reg a1 = phy addr of usr page table,
 /// finally, jump to new addr of __restore asm function
 pub fn trap_return() -> ! {
+    // set_user_trap_entry 来让应用 Trap 到 S 的时候可以跳转到 __alltraps 。注意我们把 stvec 设置为内核和应用地址空间共享的跳板页面的起始地址 TRAMPOLINE 
+    // 而不是 编译器在链接时看到的 __alltraps 的地址，因为启用分页模式之后我们只能通过跳板页面上的虚拟地址来实际取得 __alltraps 和 __restore 的汇编代码。
+    //  stvec::write(TRAMPOLINE as usize, TrapMode::Direct);
     set_user_trap_entry();
+    // __restore 需要两个参数：1. Trap 上下文在应用地址空间中的虚拟地址
+    // 2. 要继续执行的应用 地址空间的 token 下面两行则分别准备好这两个参数。
     let trap_cx_ptr = TRAP_CONTEXT_BASE;
     let user_satp = current_user_token();
     extern "C" {
@@ -112,6 +121,7 @@ pub fn trap_return() -> ! {
     // trace!("[kernel] trap_return: ..before return");
     unsafe {
         asm!(
+            //i-cache 中可能还保存着该物理页帧的 错误快照。因此我们直接将整个 i-cache 清空避免错误。
             "fence.i",
             "jr {restore_va}",         // jump to new addr of __restore asm function
             restore_va = in(reg) restore_va,
@@ -126,6 +136,7 @@ pub fn trap_return() -> ! {
 /// handle trap from kernel
 /// Unimplement: traps/interrupts/exceptions from kernel mode
 /// Todo: Chapter 9: I/O device
+/// 这里简单起见我们弱化了从 S 到 S 的 Trap ，省略了 Trap 上下文保存过程而直接 panic 。
 pub fn trap_from_kernel() -> ! {
     use riscv::register::sepc;
     trace!("stval = {:#x}, sepc = {:#x}", stval::read(), sepc::read());
