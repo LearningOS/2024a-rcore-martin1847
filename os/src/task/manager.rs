02eadbc2cce1bd5a86aca5c5d72f4f96a78094fb
martin.cong <martin.cong@zhulinkeji.com>
2024-11-03 12:22:41 +0800

    stride

--- os/src/task/manager.rs
+++ os/src/task/manager.rs
@@ -4,15 +4,15 @@
 
 use super::TaskControlBlock;
 use crate::sync::UPSafeCell;
-// use alloc::collections::binary_heap::BinaryHeap;
+use alloc::collections::binary_heap::BinaryHeap;
 // use alloc::borrow::ToOwned;
-use alloc::collections::VecDeque;
+// use alloc::collections::VecDeque;
 use alloc::sync::Arc;
 use lazy_static::*;
 ///A array of `TaskControlBlock` that is thread-safe
 pub struct TaskManager {
-    ready_queue: VecDeque<Arc<TaskControlBlock>>,
-    // ready_heap: BinaryHeap<Arc<TaskControlBlock>>,
+    // ready_queue: VecDeque<Arc<TaskControlBlock>>,
+    ready_heap: BinaryHeap<Arc<TaskControlBlock>>,
 }
 
 /// A simple FIFO scheduler.
@@ -20,43 +20,43 @@ impl TaskManager {
     ///Creat an empty TaskManager
     pub fn new() -> Self {
         Self {
-            ready_queue: VecDeque::new(),
-            // ready_heap: BinaryHeap::new()
+            // ready_queue: VecDeque::new(),
+            ready_heap: BinaryHeap::new()
         }
     }
     /// Add process back to ready queue
     pub fn add(&mut self, task: Arc<TaskControlBlock>) {
-        self.ready_queue.push_back(task);
-        // self.ready_heap.push(task);
+        // self.ready_queue.push_back(task);
+        self.ready_heap.push(task);
     }
 
     /// Take a process out of the ready queue
     pub fn fetch(&mut self) -> Option<Arc<TaskControlBlock>> {
-        // self.ready_heap.pop()
-    // }
-    // // fn fetch_by_queen() ->  Option<Arc<TaskControlBlock>>{
-        let dq = &mut self.ready_queue;
-        if dq.is_empty() {
-            return  None;
-        }
-
-        let mut min_index = 0;
-        // let mut max_index = 0;
-        for (i, value) in dq.iter().enumerate() {
-            if value.inner_readonly_access().stride < dq[min_index].inner_readonly_access().stride {
-                min_index = i;
-            }
-        }
-        
-        // warn!("found min_index stride {} -> {:?}",min_index,dq.get(min_index).unwrap().inner_readonly_access().stride);
-        dq.remove(min_index)
-        // warn!("found min_index stride {:?} / max {:?}, default : {:?}"
-        // ,dq.get(min_index).unwrap().inner_readonly_access().stride
-        // ,dq.get(max_index).unwrap().inner_readonly_access().stride
-        // ,dq.get(0).unwrap().inner_readonly_access().stride
-        // );
-        // self.ready_queue.pop_front()
+        self.ready_heap.pop()
     }
+    // // fn fetch_by_queen() ->  Option<Arc<TaskControlBlock>>{
+    //     let dq = &mut self.ready_queue;
+    //     if dq.is_empty() {
+    //         return  None;
+    //     }
+
+    //     let mut min_index = 0;
+    //     // let mut max_index = 0;
+    //     for (i, value) in dq.iter().enumerate() {
+    //         if value.inner_readonly_access().stride < dq[min_index].inner_readonly_access().stride {
+    //             min_index = i;
+    //         }
+    //     }
+        
+    //     // warn!("found min_index stride {} -> {:?}",min_index,dq.get(min_index).unwrap().inner_readonly_access().stride);
+    //     dq.remove(min_index)
+    //     // warn!("found min_index stride {:?} / max {:?}, default : {:?}"
+    //     // ,dq.get(min_index).unwrap().inner_readonly_access().stride
+    //     // ,dq.get(max_index).unwrap().inner_readonly_access().stride
+    //     // ,dq.get(0).unwrap().inner_readonly_access().stride
+    //     // );
+    //     // self.ready_queue.pop_front()
+    // }
 
     
 }
